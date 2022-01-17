af321e7d03c5276f41bd6368223888622e7d3d83
T.katoh <k4toh.tetsugaku0421@gmail.com>
2022-01-18 00:30:13 +0900

    ユーザー登録機能実装中

---
+++ cgi/html_mdset.py
@@ -0,0 +1,20 @@
+
+def htheader():
+    print("<head>")
+    print('<meta charset="Shift_JIS">')
+    print("<title>SNS for T5</title>")
+    print('<meta name="viewport" content="width=device-width, initial-scale=1">')
+    print('<link rel="stylesheet" href="../css/bootstrap.css">')
+    print('<script type="text/javascript" src="../js/jquery-3.6.0.js"></script>')
+    print('<script type="text/javascript" src="../js/bootstrap.js"></script>')
+    print('</head>')
+
+def htnavibar():
+    print('<nav class="navbar navbar-expand-lg navbar-light bg-light"><div class="container-fluid"><a class="navbar-brand" href="index.html"><img src="/gazou/T5-removebg-preview.png" alt="t5sns" width="70" height="70"></a><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#t5nav" aria-controls="t5nav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse justify-content-start" id="t5nav"><ul class="navbar-nav"><li class="nav-item"><a class="nav-link active" aria-current="page" href="../h_index.html">My Page</a></li></ul></div><div class="collapse navbar-collapse justify-content-start" id="t5nav"><ul class="navbar-nav"><li class="nav-item"><a class="nav-link active" aria-current="page" href="login.html">log in</a></li></ul></div></div></nav>')      
+                  
+def htjumbo_s():
+    print('<div class="container"><div class="bg-light p-3 p-sm-5 my-4 rounded"><div class="text-center">')
+
+def htjumbo_e():
+    print('</div></div></div>')
+  
---
+++ cgi/sign_up.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import cgi
+import cgitb
+import random
+import string
+import db_rw as dbrw
+import html_mdset as hmd
+
+#password 生成
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+
+#エラーをブラウザ上で見れるようにする
+cgitb.enable()
+
+# HTML is following
+print("Content-Type: text/html")    
+print("")
+
+form = cgi.FieldStorage()
+password = get_random_string(5)
+
+dbrw.makedb(form["user_name"].value)
+
+hmd.htheader()
+hmd.htnavibar()
+
+hmd.htjumbo_s()
+print('<h1>あなたのパスワードは</h1>')
+print('<h2>', password, '</h2>')
+print('<p> メモしてください 再発行は不可能です </p>')
